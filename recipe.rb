dd6493e0babc2e0110bd8e90a8aa1e0ff355fbc2
yonezawa <ryota_yonezawa@ipc.i.u-tokyo.ac.jp>
2014-07-06 11:59:37 +0900

    行指定

--- recipe.rb
+++ recipe.rb
@@ -1,7 +1,13 @@
-f=open(ARGV[0])
-i=1
-while line=f.gets
-	print i.to_s+":"+line
-	i+=1
+filename=ARGV[0]
+line_num=ARGV[1].to_i
+f=open(filename)
+if ARGV.length==1
+	while line=f.gets
+		print f.lineno.to_s+":"+line
+	end
 end
+if ARGV.length==2
+ print line_num.to_s+":"+f.readlines[line_num-1]	
+end
+
 f.close
